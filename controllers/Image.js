bec8e8e0a13ebc2b622d8fe9f1f31ef0b790bb7d
MoniqueCastro <moniquelimacastro@gmail.com>
2020-02-16 11:29:24 +0100

    add base project

---
+++ controllers/Image.js
@@ -0,0 +1,31 @@
+// const Clarifai = require("clarifai");
+
+// //You must add your own API key here from Clarifai.
+// const app = new Clarifai.App({
+//   apiKey: "af9e5bf704cc41288d3682b40402f32d"
+// });
+
+// const handleApiCall = (req, res) => {
+//   app.models
+//     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+//     .then(data => {
+//       res.json(data);
+//     })
+//     .catch(err => res.status(400).json("unable to work with API"));
+// };
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json("unable to get entries"));
+};
+
+module.exports = {
+  handleImage
+};
---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
+const knex = require("knex");
+
+const register = require("./controllers/Register");
+const signin = require("./controllers/Signin");
+const profile = require("./controllers/Profile");
+const image = require("./controllers/Image");
+
+const db = knex({
+  client: "pg",
+  version: "7.2",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "vcdeviasaber",
+    database: "facerecognition"
+  }
+});
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send(db.users);
+});
+
+app.post("/signin", signin.handleSignin(db, bcrypt));
+
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
+
+// app.post("/imageurl", (req, res) => {
+//   image.handleApiCall(req, res);
+// });
+
+app.listen(3001, () => {
+  console.log("app is running on port 3001");
+});
